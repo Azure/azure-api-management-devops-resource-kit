7c503eb06739c205dd331b36fbae20554d9b37d5
Lucas Huet-Hudson <lucashh@microsoft.com>
2019-01-03 11:20:37 -0500

    Lucashh creatorconfig (#56)
    * write swagger content and CLI args to API template, remove all other logic
    
    * convert cli args to config.yml, parse yml with ymldotnet
    
    * move creator-relevant logic into folder, add config YAML examples
    
    * add testing for creator cli options
    
    * update file paths for cli create tests

---
+++ src/APIM_ARMTemplate/apimtemplate/Creator/FileHandlers/ARMTemplateWriter.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System;
+
+namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates
+{
+    public class ARMTemplateWriter
+    {
+        public void WriteJSONToFile(JObject json, string location)
+        {
+            using (StreamWriter file = File.CreateText(location))
+            using (JsonTextWriter writer = new JsonTextWriter(file))
+            {
+                json.WriteTo(writer);
+            }
+        }
+
+        public void WriteAPITemplateToFile(APITemplate template, string location)
+        {
+            WriteJSONToFile(JObject.FromObject(template), String.Concat(location, @"\APITemplate.json"));
+        }
+    }
+}
