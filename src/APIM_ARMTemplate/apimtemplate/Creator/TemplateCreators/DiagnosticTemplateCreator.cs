20d548d9635f1f334ee9b1fd63603c14703fcc9e
lucashuet93 <lucashh@microsoft.com>
2019-02-14 18:56:35 -0500

    remove name property from creator diagnostic

--- src/APIM_ARMTemplate/apimtemplate/Creator/TemplateCreators/DiagnosticTemplateCreator.cs
+++ src/APIM_ARMTemplate/apimtemplate/Creator/TemplateCreators/DiagnosticTemplateCreator.cs
@@ -12,7 +12,15 @@ namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Create
                 name = $"[concat(parameters('ApimServiceName'), '/{creatorConfig.api.name}/{creatorConfig.api.diagnostic.name}')]",
                 type = "Microsoft.ApiManagement/service/apis/diagnostics",
                 apiVersion = "2018-06-01-preview",
-                properties = creatorConfig.api.diagnostic,
+                properties = new DiagnosticTemplateProperties()
+                {
+                    alwaysLog = creatorConfig.api.diagnostic.alwaysLog,
+                    loggerId = creatorConfig.api.diagnostic.loggerId,
+                    sampling = creatorConfig.api.diagnostic.sampling,
+                    frontend = creatorConfig.api.diagnostic.frontend,
+                    backend = creatorConfig.api.diagnostic.backend,
+                    enableHttpCorrelationHeaders = creatorConfig.api.diagnostic.enableHttpCorrelationHeaders
+                },
                 dependsOn = dependsOn
             };
             return diagnosticTemplateResource;
