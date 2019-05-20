21274df74bff7e6a4a182e7aff59c855e85b5f4e
lucashuet93 <lucashh@microsoft.com>
2019-05-20 10:40:30 -0400

    add release template creator

---
+++ src/APIM_ARMTemplate/apimtemplate/Creator/TemplateCreators/ReleaseTemplateCreator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Management.ApiManagement.ArmTemplates.Common;
+
+namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Create
+{
+    public class ReleaseTemplateCreator
+    {
+        public ReleaseTemplateResource CreateAPIReleaseTemplateResource(APIConfig api, string[] dependsOn)
+        {
+            string releaseName = $"release-revision-{api.apiRevision}";
+            // create release resource with properties
+            ReleaseTemplateResource releaseTemplateResource = new ReleaseTemplateResource()
+            {
+                name = $"[concat(parameters('ApimServiceName'), '/{api.name}/{releaseName}')]",
+                type = ResourceTypeConstants.APIRelease,
+                apiVersion = GlobalConstants.APIVersion,
+                properties = new ReleaseTemplateProperties()
+                {
+                    notes = $"Release created to make revision {api.apiRevision} current."
+                },
+                dependsOn = dependsOn
+            };
+            return releaseTemplateResource;
+        }
+    }
+}
