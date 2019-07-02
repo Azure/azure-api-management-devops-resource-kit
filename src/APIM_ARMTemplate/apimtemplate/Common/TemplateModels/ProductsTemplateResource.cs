e2a24f909860c1077d8730e80689837b9c29485c
Miao Jiang <mijiang@microsoft.com>
2019-07-02 16:56:09 -0700

    Do not serialize two properties by subscriptionRequired is false

--- src/APIM_ARMTemplate/apimtemplate/Common/TemplateModels/ProductsTemplateResource.cs
+++ src/APIM_ARMTemplate/apimtemplate/Common/TemplateModels/ProductsTemplateResource.cs
@@ -15,5 +15,16 @@ namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Common
         public int subscriptionsLimit { get; set; }
         public string state { get; set; }
         public string displayName { get; set; }
+
+        // This properties should not be serialized if subscriptionRequired is false
+        public bool ShouldSerializeapprovalRequired(){
+            return this.subscriptionRequired;
+        }
+
+        // This properties should not be serialized if subscriptionRequired is false
+        public bool ShouldSerializesubscriptionsLimit(){
+            return this.subscriptionRequired;
+        }
+
     }
 }
