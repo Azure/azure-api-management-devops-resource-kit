d1b043c19a91f075410c79420e9d7d00b25887f3
Umair Syed <umsyed@deloitte.com.au>
2019-09-30 15:37:11 +1000

    updates to sh

--- src/APIM_ARMTemplate/apimtemplate/runExtractor-dv1.sh
+++ src/APIM_ARMTemplate/apimtemplate/runExtractor-dv1.sh
@@ -10,4 +10,8 @@ echo Enter output folder:
 
 read outputFolder
 
+az.cmd login
+
+az.cmd account set --subscription f4c0ab08-940f-4f65-90d2-d49906c4ca17
+
 dotnet run extract --sourceApimName it-shared-apim-epa-vic-dv1 --destinationApimName it-shared-apim-epa-vic-dev --resourceGroup it-shared-integ-rg-epa-vic-dev --fileFolder "$outputFolder" --apiName "$apiName" --policyXMLBaseUrl https://deploystintegvicdev.blob.core.windows.net/deploysecurityv1apidef/_api-security-v1-features/drop/policies --linkedTemplatesBaseUrl https://deploystintegvicdev.blob.core.windows.net/deploysecurityv1apidef/_api-security-v1-features/drop/arm-files
