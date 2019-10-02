ec161fab8a91d19e8547682de5152a6d49715e99
Umair Syed <umsyed@deloitte.com.au>
2019-10-02 11:06:25 +1000

    Revert "adding cmd"
    This reverts commit acd96d59db9ccd7dfb7552394aea4b46708ea191.

--- src/APIM_ARMTemplate/apimtemplate/runExtractor-dv1.cmd
+++
@@ -1,6 +0,0 @@
-@rem extract ARM templates from dv1. Example of apiName=api-security-v1, outputFolder=C:\Dev\EPA\api-security-v1\apis\arm-files
-@ECHO on
-set /p apiName=Enter API Name : 
-set /p outputFolder=Enter Output folder: 
-dotnet run extract --sourceApimName it-shared-apim-epa-vic-dv1 --destinationApimName it-shared-apim-epa-vic-dev --resourceGroup it-shared-integ-rg-epa-vic-dev --fileFolder "%outputFolder%" --apiName "%apiName%" --policyXMLBaseUrl https://deploystintegvicdev.blob.core.windows.net/deploysecurityv1apidef/_api-security-v1-features/drop/policies --linkedTemplatesBaseUrl https://deploystintegvicdev.blob.core.windows.net/deploysecurityv1apidef/_api-security-v1-features/drop/arm-files
-pause
