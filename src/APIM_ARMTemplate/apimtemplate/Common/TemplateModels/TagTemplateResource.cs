81dec43ca7c65be8e325162cc9b8d0950f011b4b
Bablu Lawrence <bablu.lawrence@outlook.com>
2019-10-07 11:56:26 +0530

    Added template class for Tags

---
+++ src/APIM_ARMTemplate/apimtemplate/Common/TemplateModels/TagTemplateResource.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Common
+{
+
+    public class TagTemplateResource : APITemplateSubResource
+    {
+        public PolicyTemplateProperties properties { get; set; }
+    }
+
+    public class TagTemplateProperties
+    { 
+    }
+}
