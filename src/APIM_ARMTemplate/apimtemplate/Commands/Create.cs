51c4119575d402fb82125a3c893bda68efdd752f
Wayne Straub <waynestraub@gmail.com>
2020-02-04 22:27:12 -0500

    generated filename changes: if baseFileName is blank, skip "-", remove apim instance name from api filename

--- src/APIM_ARMTemplate/apimtemplate/Commands/Create.cs
+++ src/APIM_ARMTemplate/apimtemplate/Commands/Create.cs
@@ -115,7 +115,7 @@ namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Create
                         APITemplateResource apiResource = apiTemplate.resources.FirstOrDefault(resource => resource.type == ResourceTypeConstants.API) as APITemplateResource;
                         APIConfig providedAPIConfiguration = creatorConfig.apis.FirstOrDefault(api => apiResource.name.Contains(api.name, StringComparison.Ordinal));
                         // if the api version is not null the api is split into multiple templates. If the template is split and the content value has been set, then the template is for a subsequent api
-                        string apiFileName = fileNameGenerator.GenerateCreatorAPIFileName(providedAPIConfiguration.name, apiTemplateCreator.isSplitAPI(providedAPIConfiguration), apiResource.properties.value != null, creatorConfig.apimServiceName);
+                        string apiFileName = fileNameGenerator.GenerateCreatorAPIFileName(providedAPIConfiguration.name, apiTemplateCreator.isSplitAPI(providedAPIConfiguration), apiResource.properties.value != null);
                         fileWriter.WriteJSONToFile(apiTemplate, String.Concat(creatorConfig.outputLocation, apiFileName));
                     }
                     if (globalServicePolicyTemplate != null)
