47aaa03584d2d2076857a7ba0e000de991cfe44e
Rupeng Liu <rupliu@microsoft.com>
2021-02-23 13:49:41 -0800

    Not include named value template

--- src/APIM_ARMTemplate/apimtemplate/Extractor/Models/ExtractorConfiguration.cs
+++ src/APIM_ARMTemplate/apimtemplate/Extractor/Models/ExtractorConfiguration.cs
@@ -46,6 +46,9 @@ namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Extract
         public string paramLogResourceId { get; set; }
         [Description("Specify the the base url for calling api management")]
         public string serviceBaseUrl { get; set; }
+        [Description("Should include named values template")]
+        public string notIncludeNamedValue { get; set; }
+
         public void Validate()
         {
             if (string.IsNullOrEmpty(sourceApimName)) throw new ArgumentException("Missing parameter <sourceApimName>.");
@@ -104,6 +107,7 @@ namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Extract
         public bool paramNamedValue { get; private set; }
         public bool paramApiLoggerId { get; private set; }
         public bool paramLogResourceId { get; private set; }
+        public bool notIncludeNamedValue { get; private set; }
 
         public Extractor(ExtractorConfig exc, string dirName)
         {
@@ -123,6 +127,7 @@ namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Extract
             this.paramNamedValue = exc.paramNamedValue != null && exc.paramNamedValue.Equals("true");
             this.paramApiLoggerId = exc.paramApiLoggerId != null && exc.paramApiLoggerId.Equals("true");
             this.paramLogResourceId = exc.paramLogResourceId != null && exc.paramLogResourceId.Equals("true");
+            this.notIncludeNamedValue = exc.notIncludeNamedValue != null && exc.notIncludeNamedValue.Equals("true");
         }
 
         public Extractor(ExtractorConfig exc) : this(exc, exc.fileFolder)
