2a4014249b1bf98efce82c4e06d080b6fda33aab
MikeLarah <mike.larah@endjin.com>
2021-04-21 04:00:07 +0100

    Remove 'properties' property from ProductAPITemplateResource (#481)

--- src/APIM_ARMTemplate/apimtemplate/Common/TemplateModels/ProductAPITemplateResource.cs
+++ src/APIM_ARMTemplate/apimtemplate/Common/TemplateModels/ProductAPITemplateResource.cs
@@ -3,8 +3,5 @@ namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Common
 {
     public class ProductAPITemplateResource : TemplateResource
     {
-        public ProductAPITemplateProperties properties { get; set; }
     }
-
-    public class ProductAPITemplateProperties { }
 }
