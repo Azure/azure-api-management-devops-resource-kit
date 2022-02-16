e57a3126cc2919bac172bb2b4a6922a89a33b132
Dmitrii Korolev <dmkorolev@microsoft.com>
2022-02-16 10:55:32 +0100

    add issue link to TODO for creator executor

--- src/ArmTemplates/Commands/Executors/CreatorExecutor.cs
+++ src/ArmTemplates/Commands/Executors/CreatorExecutor.cs
@@ -2,6 +2,7 @@
 {
     class CreatorExecutor
     {
-        // TODO refactor creatorApplicationCommand like ExtractorExecutor is
+        // TODO refactor creatorApplicationCommand like ExtractorExecutor
+        // Issue: https://github.com/Azure/azure-api-management-devops-resource-kit/issues/623
     }
 }
