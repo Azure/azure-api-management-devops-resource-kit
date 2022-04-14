2ac2b58abe43553cdc0f7d48b673aaa35ad5bd80
cristicazan <a@b.c>
2022-04-14 16:06:27 +0300

    Fix ARM template validaiton failing with 'Microsoft.ApiManagement/service/apis/operations' requires '3' resource name argument(s)

--- src/ArmTemplates/Extractor/EntityExtractors/ApiOperationExtractor.cs
+++ src/ArmTemplates/Extractor/EntityExtractors/ApiOperationExtractor.cs
@@ -45,7 +45,7 @@ namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Extractor.Entity
                 apiOperation.OriginalName = apiOperation.Name;
 
                 apiOperation.Name = $"[concat(parameters('{ParameterNames.ApimServiceName}'), '/{apiName}/{apiOperation.OriginalName}')]";
-                apiOperation.DependsOn = new[]{ $"[resourceId('Microsoft.ApiManagement/service/apis/operations', parameters('{ParameterNames.ApimServiceName}'), '{apiName}')]" };
+                apiOperation.DependsOn = new[]{ $"[resourceId('Microsoft.ApiManagement/service/apis', parameters('{ParameterNames.ApimServiceName}'), '{apiName}')]" };
                 apiOperation.ApiVersion = GlobalConstants.ApiVersion;
                 apiOperation.Scale = null;
 
