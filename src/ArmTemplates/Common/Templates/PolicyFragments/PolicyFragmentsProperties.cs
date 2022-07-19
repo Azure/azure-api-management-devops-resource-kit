51a6e27929bec4cecf6be09da85872d6bbe403e9
Farhad Alizada <falizada@microsoft.com>
2022-07-19 14:19:05 +0200

    Add policy-fragments extraction

---
+++ src/ArmTemplates/Common/Templates/PolicyFragments/PolicyFragmentsProperties.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+// --------------------------------------------------------------------------
+
+namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Common.Templates.PolicyFragments
+{
+    public class PolicyFragmentsProperties
+    {
+        public string Description { get; set; }
+
+        public string Format { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ src/ArmTemplates/Common/Templates/PolicyFragments/PolicyFragmentsResources.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+// --------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Microsoft.Azure.Management.ApiManagement.ArmTemplates.Common.Extensions;
+using Microsoft.Azure.Management.ApiManagement.ArmTemplates.Common.Templates.Abstractions;
+
+namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Common.Templates.PolicyFragments
+{
+    public class PolicyFragmentsResources : ITemplateResources
+    {
+        public List<PolicyFragmentsResource> PolicyFragments { get; set; } = new();
+
+        public TemplateResource[] BuildTemplateResources()
+        {
+            return this.PolicyFragments.ToArray();
+        }
+
+        public bool HasContent()
+        {
+            return !this.PolicyFragments.IsNullOrEmpty();
+        }
+    }
+}
